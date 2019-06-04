7805c3f75e9a2e7552472dea2fbc7d567366c042
Gena Makhomed <makhomed@gmail.com>
2019-06-04 23:15:01 +0300

    fix debug mode

--- possible/debugger.py
+++ possible/debugger.py
@@ -11,10 +11,16 @@ class Debug(metaclass=Singleton):
             return self.__debug
 
         def enable(self):
-            self.debug = True
+            self.__debug = True
 
         def disable(self):
-            self.debug = False
+            self.__debug = False
+
+        def __str__(self):
+            if self.__debug:
+                return "debug enabled"
+            else:
+                return "debug disabled"
 
 debug = Debug()
 
