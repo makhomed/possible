2eb06a0bf96a0dadf6d80c2534458deaa86bff35
Gena Makhomed <makhomed@gmail.com>
2023-06-07 21:06:08 +0300

    add trim_blocks=True, lstrip_blocks=True to Jinja2 templates

--- possible/templates.py
+++ possible/templates.py
@@ -5,12 +5,12 @@ from possible.engine import runtime
 
 
 def render_template(template_filename, *args, **kwargs):
-    environment = Environment(loader=FileSystemLoader(runtime.config.files), keep_trailing_newline=True)
+    environment = Environment(loader=FileSystemLoader(runtime.config.files), keep_trailing_newline=True, trim_blocks=True, lstrip_blocks=True)
     template = environment.get_template(template_filename)
     return template.render(*args, **kwargs)
 
 
 def render(template_string, *args, **kwargs):
-    environment = Environment(loader=BaseLoader(), keep_trailing_newline=True)
+    environment = Environment(loader=BaseLoader(), keep_trailing_newline=True, trim_blocks=True, lstrip_blocks=True)
     template = environment.from_string(template_string)
     return template.render(*args, **kwargs)
